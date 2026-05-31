9402f34a631a45eefbacf3f13528cd264d1de830
weathe-r <brandoncseery@gmail.com>
2026-05-31 10:20:15 +0100

    Update bot activity name in bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        name: "Shoot Up The O'Block Simulator,",
         // Activity type number (0 = Playing).
         type: 0, 
       },
